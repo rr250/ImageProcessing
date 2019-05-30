3f85cfaf537b8a160e2d402313f8025f1b95f266
Rishabh Ranjan <rr250@snu.edu.in>
2019-05-30 17:22:30 +0530

    Add files via upload

--- Image Processing-2/Image_Processing-2.py
+++ Image Processing-2/Image_Processing-2.py
@@ -39,26 +39,28 @@ def roi_per_csv(image, csvfile, inpath, outpath, cwd):
         for row in csv_reader:
             cord=row[2]
             filename=row[0]
+            if not os.path.exists(os.path.join(outpath,csvfile)):
+                os.makedirs(os.path.join(outpath,csvfile))
             if '-' in cord:
                 mcords=cord.split("-")
                 subfilenames=row[4].split("?")
                 subfilenames.pop()
-                if not os.path.exists(os.path.join(outpath,filename)):
-                    os.makedirs(os.path.join(outpath,filename))
+                if not os.path.exists(os.path.join(outpath,csvfile,filename)):
+                    os.makedirs(os.path.join(outpath,csvfile,filename))
                 i=0
                 for mcord in mcords:
                     x,y,w,h = list(map(int, mcord.split(",")))
                     sub_img = image[y:y+h,x:x+w]
                     if subfilenames[0]=='':
-                        cv2.imwrite(os.path.join(cwd,outpath,filename,str(i)+'.jpg'), sub_img)
+                        cv2.imwrite(os.path.join(cwd,outpath,csvfile,filename,str(i)+'.jpg'), sub_img)
                     else:
-                        cv2.imwrite(os.path.join(cwd,outpath,filename,subfilenames[i]+'.jpg'), sub_img)
+                        cv2.imwrite(os.path.join(cwd,outpath,csvfile,filename,subfilenames[i]+'.jpg'), sub_img)
                         
                     i+=1
             else:
                 x,y,w,h = list(map(int, cord.split(",")))
                 sub_img = image[y:y+h,x:x+w]
-                cv2.imwrite(os.path.join(cwd,outpath,filename+'.jpg'), sub_img)
+                cv2.imwrite(os.path.join(cwd,outpath,csvfile,filename+'.jpg'), sub_img)
     except Exception as e:
         logging.error("Exception occurred", exc_info=True)
  
@@ -67,7 +69,7 @@ try:
     outpath=sys.argv[2]
     if not os.path.exists(outpath):
         os.makedirs(outpath)
-        logging.basicConfig(filename=os.path.join(outpath,'app.log'), filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename=os.path.join(outpath,'app.log'), filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 except Exception:
    logging.error("Exception occurred")
 
